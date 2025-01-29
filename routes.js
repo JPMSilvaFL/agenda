26f88edd9bce4d1a7212cf8d82ed0f8015f3d4cf
JPMSilvaFL <joao.pedro.69461@gmail.com>
2025-01-29 12:56:14 -0300

    Front-end montado

--- routes.js
+++ routes.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const route = express.Router();
 const homeController = require('./src/controllers/homeController');
-const contatoController = require('./src/controllers/contatoController');
+const erroController = require('./src/controllers/404Controller');
+const loginController = require('./src/controllers/loginController');
+const registerControler = require('./src/controllers/registerControler');
+const cadastroContatoController = require('./src/controllers/cadastroContatoController');
 
 // Rotas da home
 route.get('/', homeController.paginaInicial);
 route.post('/', homeController.trataPost);
 
 // Rotas de contato
-route.get('/contato', contatoController.paginaInicial);
+route.get('/404', erroController.paginaInicial);
+route.get('/login', loginController.paginaInicial);
+route.get('/register', registerControler.paginaInicial);
+route.get('/cadastroContato', cadastroContatoController.paginaInicial);
 
 
 module.exports = route;
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,41 @@
+const { query } = require('express');
 const path = require('path'); // CommonJS
+const { TLSSocket } = require('tls');
+
 
 module.exports = {
   mode: 'production',
-  entry: './frontend/main.js',
+  entry: './server.js',
   output: {
     path: path.resolve(__dirname, 'public', 'assets', 'js'),
     filename: 'bundle.js'
   },
+  // resolve: {
+  //   fallback: {
+  //     path: require.resolve("path-browserify"),
+  //     crypto: false,
+  //     fs: false,
+  //     http: false,
+  //     https: false,
+  //     os: false,
+  //     stream: false,
+  //     util: false,
+  //     zlib: false,
+  //     timers: false,
+  //     dns: false,
+  //     tls: false,
+  //     net: false,
+  //     querystring: false,
+  //     console: false
+
+
+  //   }
+  // },
   module: {
+
     rules: [{
       exclude: /node_modules/,
-      test: /\.js$/,
+      test: /\.js | .ejs$/,
       use: {
         loader: 'babel-loader',
         options: {
@@ -22,5 +47,6 @@ module.exports = {
       use: ['style-loader', 'css-loader']
     }]
   },
+
   devtool: 'source-map'
 };
